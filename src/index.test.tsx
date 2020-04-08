bd1486755d9391ed647ed406a04312640029710a
Carlo Jeske <jeske@sc-networks.com>
2020-04-08 11:01:32 +0200

    [namespace] implemented loading from namespaces and cache them and show them

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,17 +1,22 @@
-import plusnew, { component } from "@plusnew/core";
+import plusnew, { component, store } from "@plusnew/core";
 import enzymeAdapterPlusnew, { mount } from "@plusnew/enzyme-adapter";
 import { configure } from "enzyme";
 import i18nFactory from "./index";
 
 configure({ adapter: new enzymeAdapterPlusnew() });
 
+function nextTick() {
+  return new Promise((resolve) => resolve());
+}
+
 describe("test dragFactory", () => {
-  it("dragState is shown correctly", () => {
+  it("dragState is shown correctly", async () => {
     const translations = {
       cd: jest.fn((language: string) =>
         Promise.resolve({
           foo: {
-            bar: `foobar-${language}`,
+            bar: `bar-${language}`,
+            baz: `baz-${language}`,
           },
         })
       ),
@@ -20,18 +25,56 @@ describe("test dragFactory", () => {
       translations,
     });
 
+    const language = store("de");
+
     const MainComponent = component("MainComponent", () => (
-      <>
-        <i18n.Provider language="de">
-          <i18n.Consumer>
-            {({ cd }) => <span>{cd()?.foo.bar}</span>}
-          </i18n.Consumer>
-        </i18n.Provider>
-      </>
+      <language.Observer>
+        {(languageState) => (
+          <i18n.Provider language={languageState}>
+            <i18n.Consumer>
+              {({ cd }) => <span>{cd()?.foo.bar ?? "loading"}</span>}
+            </i18n.Consumer>
+            <i18n.Consumer>
+              {({ cd }) => <div>{cd()?.foo.baz ?? "loading"}</div>}
+            </i18n.Consumer>
+          </i18n.Provider>
+        )}
+      </language.Observer>
     ));
 
     const wrapper = mount(<MainComponent />);
 
-    expect(wrapper.contains(<span>foobar-de</span>)).toBe(true);
+    // When initially cd() is called, the function should return null and call the settings.cd(de)
+    expect(wrapper.contains(<span>loading</span>)).toBe(true);
+    expect(wrapper.contains(<div>loading</div>)).toBe(true);
+    expect(translations.cd).toHaveBeenCalledTimes(1);
+
+    await nextTick();
+
+    // When the promise is resolved, the component should be updated
+    expect(wrapper.contains(<span>bar-de</span>)).toBe(true);
+    expect(wrapper.contains(<div>baz-de</div>)).toBe(true);
+    expect(translations.cd).toHaveBeenCalledTimes(1);
+
+    language.dispatch("en");
+
+    // When language got changed, the function should return null and call the settings.cd(en)
+    expect(wrapper.contains(<span>loading</span>)).toBe(true);
+    expect(wrapper.contains(<div>loading</div>)).toBe(true);
+    expect(translations.cd).toHaveBeenCalledTimes(2);
+
+    await nextTick();
+
+    // When the promise is resolved, the component should be updated
+    expect(wrapper.contains(<span>bar-en</span>)).toBe(true);
+    expect(wrapper.contains(<div>baz-en</div>)).toBe(true);
+    expect(translations.cd).toHaveBeenCalledTimes(2);
+
+    language.dispatch("de");
+
+    // When switched back to de, everything should be visible instantly without any new loading
+    expect(wrapper.contains(<span>bar-de</span>)).toBe(true);
+    expect(wrapper.contains(<div>baz-de</div>)).toBe(true);
+    expect(translations.cd).toHaveBeenCalledTimes(2);
   });
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
-import { Component, ApplicationElement } from "@plusnew/core";
-
-type PromiseType<P extends Promise<any>> = P extends Promise<infer A>
-  ? A
-  : never;
+import plusnew, {
+  Component,
+  ApplicationElement,
+  Props,
+  store,
+  context,
+} from "@plusnew/core";
+import { PromiseType, mapObject } from "util/functional";
 
 type settingsTemplate = {
   translations: {
@@ -25,16 +28,122 @@ type consumerProps<T extends settingsTemplate> = {
   children: (settings: resolvedLanguage<T>) => {};
 };
 
-export default function factory<T extends settingsTemplate>(_settings: T) {
+type languageCache<T extends settingsTemplate> = {
+  [language: string]: Partial<
+    {
+      [namespace in keyof T["translations"]]:
+        | {
+            error: false;
+            data: PromiseType<ReturnType<T["translations"][namespace]>>;
+          }
+        | { error: true };
+    }
+  >;
+};
+
+export default function factory<T extends settingsTemplate>(settings: T) {
+  const i18n = context<
+    { cachedLanguages: languageCache<T>; currentLanguage: string },
+    string
+  >();
+
+  const loadingNamespaces: { namespace: string; language: string }[] = [];
+
   return {
     Provider: class Provider extends Component<providerProps> {
-      render() {
-        return null;
+      render(Props: Props<providerProps>) {
+        const cachedLanguages = store<
+          languageCache<T>,
+          { language: string; namespace: string; data: any }
+        >({}, (currentState, action) => {
+          return {
+            ...currentState,
+            [action.language]: {
+              ...currentState[action.language],
+              [action.namespace]: {
+                error: false,
+                data: action.data,
+              },
+            },
+          };
+        });
+
+        function loadNamespace(namespace: string) {
+          const currentLanguage = Props.getState().language;
+
+          if (
+            loadingNamespaces.find(
+              (loadingNamespace) =>
+                loadingNamespace.language === currentLanguage &&
+                loadingNamespace.namespace === namespace
+            ) === undefined
+          ) {
+            loadingNamespaces.push({ namespace, language: currentLanguage });
+
+            settings.translations[namespace](currentLanguage).then((data) => {
+              cachedLanguages.dispatch({
+                data,
+                language: currentLanguage,
+                namespace,
+              });
+            });
+          }
+        }
+        return (
+          <cachedLanguages.Observer>
+            {(cachedLanguagesState) => (
+              <Props>
+                {(props) => (
+                  <i18n.Provider
+                    state={{
+                      cachedLanguages: cachedLanguagesState,
+                      currentLanguage: props.language,
+                    }}
+                    dispatch={loadNamespace}
+                  >
+                    {props.children}
+                  </i18n.Provider>
+                )}
+              </Props>
+            )}
+          </cachedLanguages.Observer>
+        );
       }
     },
     Consumer: class Consumer extends Component<consumerProps<T>> {
-      render() {
-        return null;
+      render(Props: Props<consumerProps<T>>) {
+        return (
+          <i18n.Consumer>
+            {(i18nState, loadNamespace) => {
+              const settingsContainer = mapObject(
+                settings.translations,
+                (_namespace, namespaceName) => {
+                  i18nState;
+
+                  return () => {
+                    if (
+                      i18nState.cachedLanguages[i18nState.currentLanguage]?.[
+                        namespaceName
+                      ]
+                    ) {
+                      return (i18nState.cachedLanguages[
+                        i18nState.currentLanguage
+                      ][namespaceName] as any).data;
+                    }
+                    loadNamespace(namespaceName as string);
+
+                    return null;
+                  };
+                }
+              );
+              return (
+                <Props>
+                  {(props) => (props.children as any)[0](settingsContainer)}
+                </Props>
+              );
+            }}
+          </i18n.Consumer>
+        );
       }
     },
   };
